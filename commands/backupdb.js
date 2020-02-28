5df966b8bf1a165bbbc85098edc553c0b5bdbeb7
Matthew <savageguy@gmail.com>
2020-02-28 15:58:02 +1300

    Only allowing me to backup database

--- commands/backupdb.js
+++ commands/backupdb.js
@@ -1,8 +1,7 @@
 module.exports = {
   name: 'backupdb',
   description: 'send a dm to admin with the database',
-  adminOnly:true,
-  guildOnly: true,
+  meOnly:true,
   execute(message, args,client) {
     message.delete().catch(err => console.log(err));
     message.author.send(new Date().toISOString(), {
