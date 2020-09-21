8e6abcacfc93d19bab74379ed9b3999df524c15d
Matthew <savageguy@gmail.com>
2020-09-22 11:58:49 +1200

    WS version

--- db/models/UserSoundboard.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-	return sequelize.define('user_soundboard', {
-		user_id: DataTypes.STRING,
-		date: DataTypes.INTEGER,
-    command: DataTypes.STRING
-	}, {
-		timestamps: false,
-	});
-};
