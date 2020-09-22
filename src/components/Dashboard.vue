e54097a76ba96614e087bc8119eb7c338e8c1212
Matthew <savageguy@gmail.com>
2020-09-22 15:24:37 +1200

    Removing unused boilerplate code

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Dashboard">
+Hi
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
